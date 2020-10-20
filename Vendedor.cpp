72283234ca5cb491e55dd50abe1181a505c7c9f0
iagosg11 <73145886+iagosg11@users.noreply.github.com>
2020-10-20 14:37:09 -0300

    Update Vendedor.cpp
    Mudanças Realizadas no Código:
    - Criação de uma função Imprimir para evitar a utilização de mais linhas do que o necessário no main().
    - A variável Nome ficou privada para tornar mais difícil o acesso e modificação.

--- Vendedor.cpp
+++ Vendedor.cpp
@@ -1,14 +1,35 @@
 #include <string>
 #include "Empregado.hpp"
 
-class Vendedor : public Empregado {
+#define Calc_Anual 12;
 
+class Vendedor : public Empregado 
+{
   public:
-    std::string nome;  	  
+   void set_nome(std::string _nome)
+   {
+    Nome = _Nome;
+   }
+  
+   std::string get_nome()
+   {
+    return Nome;
+   }  	  
 	
-	double quotaTotalAnual() {
-	  return quotaMensalVendas * 12;
-	}
+   double quotaTotalAnual() 
+   {
+    return quotaMensalVendas * Calc_Anual;
+   }
+   
+   void Imprimir(double horasTrabalhadas)
+   {
+    std::cout << "Nome: " << get_nome << std::endl;
+    std::cout << "Salario Mes: " << PagamentoMes(HorasTrabalhadas) << std::endl;
+    std::cout << "Quota vendas: " << quotaTotalAnual() << std::endl;
+   }
+   
+  private:
+   std::string _Nome;	
 	
 };
 
