021a8291cc0de78aecfcf383efbc80948ae27bc0
iagosg11 <73145886+iagosg11@users.noreply.github.com>
2020-10-20 15:17:12 -0300

    Update Vendedor.cpp

--- Vendedor.cpp
+++ Vendedor.cpp
@@ -6,30 +6,36 @@
 class Vendedor : public Empregado 
 {
   public:
-   void set_nome(std::string _nome)
+   void set_nome(std::string Nome)
    {
-    Nome = _Nome;
+    _Nome = Nome;
    }
   
    std::string get_nome()
    {
     return Nome;
-   }  	  
+   } 
+   
+   void set_QuotaMensalVendas(double QuotaMensalVendas)
+   {
+    _QuotaMensalVendas = QuotaMensalVendas;	    
+   }
 	
    double quotaTotalAnual() 
    {
-    return quotaMensalVendas * Calc_Anual;
+    return QuotaMensalVendas * Calc_Anual;
    }
    
    void Imprimir(double horasTrabalhadas)
    {
     std::cout << "Nome: " << get_nome << std::endl;
     std::cout << "Salario Mes: " << PagamentoMes(HorasTrabalhadas) << std::endl;
-    std::cout << "Quota vendas: " << quotaTotalAnual() << std::endl;
+    std::cout << "Quota vendas: " << QuotaTotalAnual() << std::endl;
    }
    
   private:
-   std::string _Nome;	
+   std::string _Nome;
+   int _QuotaMensalVendas;
 	
 };
 
