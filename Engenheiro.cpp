1a266c5f9a9d7bff13fa04614380afac714b488c
iagosg11 <73145886+iagosg11@users.noreply.github.com>
2020-10-20 13:45:45 -0300

    Update Engenheiro.cpp
    Mudanças Realizadas no Código:
    - Criação de uma função Imprimir para evitar a utilização de mais linhas do que o necessário no main().
    - As variáveis Nome e Projetos ficaram privadas para tornar mais difícil o acesso e modificação.

--- Engenheiro.cpp
+++ Engenheiro.cpp
@@ -1,11 +1,42 @@
 #include <string>
 #include "Empregado.hpp"
 
-class Engenheiro : public Empregado {
-
+class Engenheiro : public Empregado 
+{
   public:
-    std::string nome;  
-	int projetos;
+  void set_nome(std::string _nome)
+  {
+   nome = _nome;
+  }
+  
+  std::string get_nome()
+  {
+   return nome;
+  }
+  
+  void set_projetos(int _projetos)
+  {
+   projetos = _projetos;
+  }
+  
+  int get_projetos()
+  {
+   return projetos;
+  }
+  
+  void Imprimir(double HorasTrabalhadas)
+  {
+   std::cout << "Nome: " << getnome() << std::endl;
+   std::cout << "Salario Mes: " << PagamentoMes(HorasTrabalhadas) << std::endl;
+   std::cout << "Projetos: " << get_projetos() << std::endl;
+   std::cout << std::endl;
+  }
+
+  private:
+   std::string _Nome;
+   int _Projetos;
+   
+   
 	
 };
 
